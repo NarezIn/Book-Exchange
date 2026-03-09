b8160fa8ad4517e7565154b3f6036a5ecd9fd638
NarezIn <zn2133@nyu.edu>
2026-03-09 19:09:07 -0400

    Update netlify configuration

--- .netlify/functions/app.py
+++
@@ -1,5 +0,0 @@
-from serverless_wsgi import handle_request
-from app import app # the main flask file is 'app.py' and the app variable is 'app'
-
-def handler(event, context):
-    return handle_request(app, event, context)
